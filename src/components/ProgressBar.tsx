62e185ed254e2441e6bb78edf04aece1ed7c9e15
Ee Kin <wongeekinn@gmail.com>
2024-02-07 00:41:04 +0800

    finished progress bar stuff

--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,15 +1,22 @@
-
+import { useSelector } from 'react-redux';
+import { RootState } from '../store';
 const ProgressBar = () => {
+  const sections = useSelector((state: RootState) => state.section.sections);
+  const completedSections = sections.filter((section) => section.isCompleted).length;
+  const totalSections = sections.length;
+
+  const progressPercentage = (completedSections/totalSections) * 100
+  
   return (
     <div className="w-3/4 h-2 bg-gray-200 rounded-full">
       <div
         className="h-full bg-green-500 rounded-full"
         style={{
-          width: '30%',
+          width: `${progressPercentage}%`,
         }}
       />
     </div>
   );
-}
+};
 
-export default ProgressBar
+export default ProgressBar;
--- src/components/ProgressContainer.tsx
+++ src/components/ProgressContainer.tsx
@@ -1,18 +1,23 @@
-
+import { useSelector } from "react-redux";
+import { RootState } from '../store';
 interface ProgressContainerProps {
     children: React.ReactNode;
  }
 
 const ProgressContainer = ({ children }: ProgressContainerProps) => {
+  const sections = useSelector((state: RootState) => state.section.sections);
+  const completedSections = sections.filter((section) => section.isCompleted).length
+  const totalSections = sections.length;
+  
   return (
     <div className="flex flex-col items-center space-y-4 text-center">
       <div className="inline-block rounded-lg bg-gray-100 px-3 py-1 text-sm dark:bg-gray-800">
         Progress
       </div>
       <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
-        3 out of 5 sections completed
+        {completedSections} out of {totalSections} sections completed
       </h2>
-      { children }
+      {children}
     </div>
   );
 }
